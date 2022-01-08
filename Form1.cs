f88e002353c9ac1850120d9de773b5c7b9cb6087
Max Ivannikov <ivannikovmax04@gmail.com>
2022-01-09 00:53:16 +0200

    Added README

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace MilestonerApp
     public partial class Form1 : Form, Milestoner.IGui
     {
         Milestoner.Vector _Vector;
+        int[] _Resolution;
         public Form1()
         {
             _Vector = new Vector(this);
